82d7801b5cbd39b2741130fa7c25829537a459aa
Mason Bane <mbane0525@gmail.com>
2025-02-04 13:41:58 -0600

    Started HW6

--- HW6.cu
+++ HW6.cu
@@ -1,29 +1,92 @@
-//nvcc SimpleJuliaSetCPU.cu -o SimpleJuliaSetCPU -lglut -lGL -lm
-// This is a simple Julia set which is repeated iterations of 
-// Znew = Zold + C where Z and C are imaginary numbers.
-// After so many tries if Zinitial escapes color it black if it stays around color it red.
+//Name: Mason Bane
+// nvcc HW6.cu -o temp -lglut -lGL
+// glut and GL are openGL libraries.
 
-#include <GL/glut.h>
-#include <stdlib.h>
+/*
+ What to do:
+ This code displays a simple Julia fractal using the CPU.
+ Rewrite the code so that it uses the GPU to create the fractal. 
+ Keep the window at 1024 by 1024.
+*/
+
+// Include files
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
+#include <GL/glut.h>
 
-#define MAXMAG 10.0
-#define MAXITERATIONS 200
+// Defines
+#define MAXMAG 10.0 // If you grow larger than this, we assume that you have escaped.
+#define MAXITERATIONS 200 // If you have not escaped after this many attempts, we assume you are not going to escape.
+#define A  -0.824	//Real part of C
+#define B  -0.1711	//Imaginary part of C
 
-#define A  -0.824	//real
-#define B  -0.1711	//imaginary
-
+// Global variables
 unsigned int WindowWidth = 1024;
 unsigned int WindowHeight = 1024;
+int N = WindowWidth*WindowHeight; //Number of pixels
+
+dim3 BlockSize; //This variable will hold the Dimensions of your blocks
+dim3 GridSize; //This variable will hold the Dimensions of your grid
 
 float XMin = -2.0;
 float XMax =  2.0;
 float YMin = -2.0;
 float YMax =  2.0;
 
-float color (float x, float y) 
+// Function prototypes
+void cudaErrorCheck(const char*, int);
+float escapeOrNotColor(float, float);
+void setUpDevices();
+void allocateMemory();
+void initialize();
+
+// This will be the layout of the parallel space we will be using.
+
+void setUpDevices()
+{
+	//Current thoughts: 1024 rows by 1024 columns, each block is one row, each thread will be a pixel in that row.
+	//pretty convenient huh?
+	BlockSize.x = WindowWidth;
+	BlockSize.y = 1;
+	BlockSize.z = 1;
+	
+	GridSize.x = WindowHeight; // This gives us the correct number of blocks.
+	GridSize.y = 1;
+	GridSize.z = 1;
+}
+
+void cudaErrorCheck(const char *file, int line)
+{
+	cudaError_t  error;
+	error = cudaGetLastError();
+
+	if(error != cudaSuccess)
+	{
+		printf("\n CUDA ERROR: message = %s, File = %s, Line = %d\n", cudaGetErrorString(error), file, line);
+		exit(0);
+	}
+}
+
+// Allocating the memory we will be using.
+void allocateMemory()
+{	
+	// Host "CPU" memory.			
+
+	
+	// Device "GPU" Memory.
+
+
+}
+
+// Loading values into the vectors that we will add.
+void initialize()
+{
+	for(int i = 0; i < N; i++)
+	{		
+
+	}
+}
+
+float escapeOrNotColor (float x, float y) 
 {
 	float mag,tempX;
 	int count;
@@ -34,14 +97,8 @@ float color (float x, float y)
 	count = 0;
 	mag = sqrt(x*x + y*y);;
 	while (mag < maxMag && count < maxCount) 
-	{
-		//Zn = Zo*Zo + C
-		//or xn + yni = (xo + yoi)*(xo + yoi) + A + Bi
-		//xn = xo*xo - yo*yo + A (real Part)
-		//yn = 2*xo*yo + B (imagenary part)
-		
-		//We will be changing the x but we need its old value to find y.	
-		tempX = x; 
+	{	
+		tempX = x; //We will be changing the x but we need its old value to find y.
 		x = x*x - y*y + A;
 		y = (2.0 * tempX * y) + B;
 		mag = sqrt(x*x + y*y);
@@ -76,7 +133,7 @@ void display(void)
 		x = XMin;
 		while(x < XMax) 
 		{
-			pixels[k] = color(x,y);	//Red on or off returned from color
+			pixels[k] = escapeOrNotColor(x,y);	//Red on or off returned from color
 			pixels[k+1] = 0.0; 	//Green off
 			pixels[k+2] = 0.0;	//Blue off
 			k=k+3;			//Skip to next pixel (3 float jump)
@@ -95,8 +152,18 @@ int main(int argc, char** argv)
    	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
    	glutInitWindowSize(WindowWidth, WindowHeight);
-	glutCreateWindow("Fractals man, fractals");
+	glutCreateWindow("Fractals--Man--Fractals");
    	glutDisplayFunc(display);
    	glutMainLoop();
 }
+
+//process
+
+//Each thread will be a pixel and needs 3 float values for RGB, so an array of float4s of size N is needed
+//initalize the array with all black pixels (0,0,0,1)
+//transfer the memory to the GPU
+//manipulate the pixels in parallel
+//transfer the memory back to the CPU
+//display the pixels
+//FREE MEM
 
