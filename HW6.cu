94a8e18baed2588c1de893cf97c4fef12fbf9058
Mason Bane <mbane0525@gmail.com>
2025-02-06 13:47:29 -0600

    Finished?

--- HW6.cu
+++ HW6.cu
@@ -23,6 +23,7 @@
 unsigned int WindowWidth = 1024;
 unsigned int WindowHeight = 1024;
 int N = WindowWidth*WindowHeight; //Number of pixels
+float4 *Pixels_GPU, *Pixels_CPU; //This will be the array of pixels we will be using.
 
 dim3 BlockSize; //This variable will hold the Dimensions of your blocks
 dim3 GridSize; //This variable will hold the Dimensions of your grid
@@ -69,11 +70,13 @@ void cudaErrorCheck(const char *file, int line)
 // Allocating the memory we will be using.
 void allocateMemory()
 {	
-	// Host "CPU" memory.			
+	// Host "CPU" memory.
+	Pixels_CPU = (float4 *)malloc(N*sizeof(float4));
+			
 
 	
 	// Device "GPU" Memory.
-
+	cudaMalloc((void**)&Pixels_GPU, N*sizeof(float4));
 
 }
 
@@ -81,70 +84,150 @@ void allocateMemory()
 void initialize()
 {
 	for(int i = 0; i < N; i++)
-	{		
-
+	{
+		//initialize the pixels to black.
+		Pixels_CPU[i].x = 0.0;
+		Pixels_CPU[i].y = 0.0;
+		Pixels_CPU[i].z = 0.0;
+		Pixels_CPU[i].w = 1.0;
 	}
 }
 
-float escapeOrNotColor (float x, float y) 
-{
-	float mag,tempX;
-	int count;
+// float escapeOrNotColorCPU (float x, float y) 
+// {
+// 	float mag,tempX;
+// 	int count;
 	
+// 	int maxCount = MAXITERATIONS;
+// 	float maxMag = MAXMAG;
+	
+// 	count = 0;
+// 	mag = sqrt(x*x + y*y);;
+// 	while (mag < maxMag && count < maxCount) 
+// 	{	
+// 		tempX = x; //We will be changing the x but we need its old value to find y.
+// 		x = x*x - y*y + A;
+// 		y = (2.0 * tempX * y) + B;
+// 		mag = sqrt(x*x + y*y);
+// 		count++;
+// 	}
+// 	if(count < maxCount) 
+// 	{
+// 		return(0.0);
+// 	}
+// 	else
+// 	{
+// 		return(1.0);
+// 	}
+// }
+
+__global__ void escapeOrNotColorGPU(float4 *pixels, int N, int xMax, int xMin, int yMax, int yMin, int windowWidth, int windowHeight)
+{
+	//declare variables
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	float x, y, mag, tempX;
+	int count = 0;
 	int maxCount = MAXITERATIONS;
 	float maxMag = MAXMAG;
-	
-	count = 0;
-	mag = sqrt(x*x + y*y);;
-	while (mag < maxMag && count < maxCount) 
-	{	
-		tempX = x; //We will be changing the x but we need its old value to find y.
-		x = x*x - y*y + A;
-		y = (2.0 * tempX * y) + B;
+
+	float stepSizeX = (xMax - xMin)/((float)windowWidth); //step through threads / columns
+	float stepSizeY = (yMax - yMin)/((float)windowHeight); //step through blocks / rows
+
+	//rows are blocks, columns are threads
+	int row = blockIdx.x;
+	int column = threadIdx.x;
+
+
+	//calculate the x and y values for this pixel
+	x = xMin + (stepSizeX * column); //min column in coords + how far over you are in the row
+	y = yMin + (stepSizeY * row); //min row in coords + how far down you are in the row
+
+	//make sure that we don't go out of bounds
+	if(id < N)
+	{
 		mag = sqrt(x*x + y*y);
-		count++;
+		while (mag < maxMag && count < maxCount) 
+		{	
+			tempX = x; //We will be changing the x but we need its old value to find y.
+			x = x*x - y*y + A;
+			y = (2.0 * tempX * y) + B;
+			mag = sqrt(x*x + y*y);
+			count++;
+		}
+		if (count < maxCount) //if we escaped, we're black
+		{
+			pixels[id].x = 0.0;
+			pixels[id].y = 0.0;
+			pixels[id].z = 0.0;
+			pixels[id].w = 1.0;
+		}
+		else //we didn't escape, we're red
+		{
+			pixels[id].x = 1.0;
+			pixels[id].y = 0.0;
+			pixels[id].z = 0.0;
+			pixels[id].w = 1.0;
+		}
+
 	}
-	if(count < maxCount) 
-	{
-		return(0.0);
-	}
-	else
-	{
-		return(1.0);
-	}
+
+
+
 }
 
 void display(void) 
 { 
-	float *pixels; 
-	float x, y, stepSizeX, stepSizeY;
-	int k;
+	// float *pixels; 
+	// float x, y, stepSizeX, stepSizeY;
+	// int k;
 	
-	//We need the 3 because each pixel has a red, green, and blue value.
-	pixels = (float *)malloc(WindowWidth*WindowHeight*3*sizeof(float));
+	// //We need the 3 because each pixel has a red, green, and blue value.
+	// pixels = (float *)malloc(WindowWidth*WindowHeight*3*sizeof(float));
 	
-	stepSizeX = (XMax - XMin)/((float)WindowWidth);
-	stepSizeY = (YMax - YMin)/((float)WindowHeight);
+	// stepSizeX = (XMax - XMin)/((float)WindowWidth);
+	// stepSizeY = (YMax - YMin)/((float)WindowHeight);
 	
-	k=0;
-	y = YMin;
-	while(y < YMax) 
-	{
-		x = XMin;
-		while(x < XMax) 
-		{
-			pixels[k] = escapeOrNotColor(x,y);	//Red on or off returned from color
-			pixels[k+1] = 0.0; 	//Green off
-			pixels[k+2] = 0.0;	//Blue off
-			k=k+3;			//Skip to next pixel (3 float jump)
-			x += stepSizeX;
-		}
-		y += stepSizeY;
-	}
+	// k=0;
+	// y = YMin;
+	// while(y < YMax) 
+	// {
+	// 	x = XMin;
+	// 	while(x < XMax) 
+	// 	{
+	// 		pixels[k] = escapeOrNotColor(x,y);	//Red on or off returned from color
+	// 		pixels[k+1] = 0.0; 	//Green off
+	// 		pixels[k+2] = 0.0;	//Blue off
+	// 		k=k+3;			//Skip to next pixel (3 float jump)
+	// 		x += stepSizeX;
+	// 	}
+
+	// 	y += stepSizeY;
+	// }
 
 	//Putting pixels on the screen.
-	glDrawPixels(WindowWidth, WindowHeight, GL_RGB, GL_FLOAT, pixels); 
-	glFlush(); 
+	// glDrawPixels(WindowWidth, WindowHeight, GL_RGB, GL_FLOAT, pixels); 
+	// glFlush(); 
+
+	//call the kernel
+	escapeOrNotColorGPU<<<GridSize, BlockSize>>>(Pixels_GPU, N, XMax, XMin, YMax, YMin, WindowWidth, WindowHeight);
+	cudaErrorCheck(__FILE__, __LINE__);
+
+	//copy the pixels from the GPU to the CPU
+	cudaMemcpy(Pixels_CPU, Pixels_GPU, N*sizeof(float4), cudaMemcpyDeviceToHost);
+	cudaErrorCheck(__FILE__, __LINE__);
+
+	//Putting pixels on the screen.
+	glDrawPixels(WindowWidth, WindowHeight, GL_RGBA, GL_FLOAT, Pixels_CPU);
+	glFlush();
+}
+
+void cleanUp()
+{
+	free(Pixels_CPU);
+	cudaFree(Pixels_GPU);
+	printf("Memory freed. Exiting...\n");
+
+	exit(0);
 }
 
 int main(int argc, char** argv)
@@ -154,7 +237,24 @@ int main(int argc, char** argv)
    	glutInitWindowSize(WindowWidth, WindowHeight);
 	glutCreateWindow("Fractals--Man--Fractals");
    	glutDisplayFunc(display);
+
+	//when we exit the glutMainLoop, we want to free the memory.
+	atexit(cleanUp);
+
+	//set up the devices
+	setUpDevices();
+
+	//allocate the memory
+	allocateMemory();
+
+	//initialize values on the CPU
+	initialize();
+
+	//Transfer the memory to the GPU
+	cudaMemcpy(Pixels_GPU, Pixels_CPU, N*sizeof(float4), cudaMemcpyHostToDevice);
+
    	glutMainLoop();
+	
 }
 
 //process
