f36eabf306f7f75e58d12cf4e376443853ede82a
MBane04 <mbane0525@gmail.com>
2025-04-24 21:33:18 -0500

    EZ PZ Limon Squeezie

--- HW27.cu
+++ HW27.cu
@@ -10,22 +10,56 @@
 */
 
 // Include files
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 // Defines
+#define N 10000 // Number of steps
 
 
 // Globals
 
 
 // Function prototypes
+int walk(int);
 
 
-int main(int, char**);
+int walk(int steps)
+{
+	int position = 0; // Reset position for each walk
+
+	for (int i = 0; i < steps; i++)
+	{
+		// Random number -1 or 1
+		int step = (rand() % 2) * 2 - 1; // number%2 [0, 1] then multiply by 2 [0, 2] and subtract 1 [-1, 1]
+
+		//make sure the random number works
+		//printf("Step %d: %d\n", i + 1, step);
+
+		// Update position
+		position += step;
+	}
+
+	return position;
+}
+
 
 
 int main(int argc, char** argv)
 {
+	// Seed the random number generator
+	srand(time(NULL));
+
+	// Print the final position
+	printf("Final position after %d steps: %d\n", N, walk(N));
+
+	//do it in a loop for fun
+	// for (int i = 0; i < 100000; i++)
+	// {
+	// 	printf("Final position after %d steps: %d\n", N, walk(N));
+	// }
 	
 	return 0;
 }
